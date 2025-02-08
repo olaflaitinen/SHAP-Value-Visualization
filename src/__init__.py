61c5a8ff5493fdcbed42ea3979971f4abd3f830a
Olaf Laitinen <olaf.laitinen@gmail.com>
2025-02-08 17:23:32 +0100

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+"""
+SHAP Value Visualization package.
+
+This package contains modules for loading data, training a decision tree model,
+and visualizing SHAP values for model interpretability.
+"""
